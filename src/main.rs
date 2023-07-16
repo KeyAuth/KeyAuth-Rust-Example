f60e155cd6b0db7f87b56647c83e8a5697f6190c
mazkdevf <79049205+mazkdevf@users.noreply.github.com>
2023-07-16 16:43:46 +0300

    Changed to example appdetails.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 fn main() {
     let mut keyauthapp = keyauth::v1_2::KeyauthApi::new(
-        "library-development",
-        "EdmsTKiuld",
-        "9f752b6a414455175efd942abfd2183667413d57b1d59d6742d8437c71802b49",
+        "example",
+        "JjPMBVlIOd",
+        "db40d586f4b189e04e5c18c3c94b7e72221be3f6551995adc05236948d1762bc",
         "1.0",
         "https://keyauth.cc/api/1.2/",
     );
